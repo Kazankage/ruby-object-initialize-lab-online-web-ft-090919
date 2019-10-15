b06059daaa86e75efd34f026135948a76768937c
Randel Jiao <randeljiao@gmail.com>
2019-10-15 05:50:55 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -1,5 +1,19 @@
 class Person
   def initialize(name)
-    name
+    @name = name
   end
+  
+  attr_accessor = :name
 end
+
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name 
+    @breed = breed 
+  end
+  
+  attr_accessor = :name, :breed
+end
+
+george = Person.new("George")
+puts george.name 
